dbde1f82b759f08e4eb8ffdb271dd514686ec7cb
Adrian Cerdeira <acwebdesign01@gmail.com>
2020-06-08 18:06:44 +0200

    use webpack for scss etc

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpeg?|svg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            // favicon: 'src/favicon.ico',
+            inlineSource: '.(js|css)$', // embed all javascript and css inline needs inline-source-plugin
+            minify: {
+                collapseWhitespace: true
+            }
+        }),
+    ]
+};
