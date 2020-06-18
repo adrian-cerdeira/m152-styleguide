0eaf161153140c4e9ceff1c4e51068adf7ae9170
Adrian Cerdeira <acwebdesign01@gmail.com>
2020-06-18 11:03:26 +0200

    install smooth scroll and added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import SmoothScroll from '../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.js';
 import './index.scss';
+
+// Smooth Scroll
+new SmoothScroll('a[href*="#"]', {
+    speed: 1100,
+    speedAsDuration: true
+});
